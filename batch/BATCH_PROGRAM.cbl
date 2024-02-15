004808360aee743e3ae0e6aff4af2bf324fd4f8b
fin8 cloud <fin8cloud@gmail.com>
2024-02-15 16:08:02 +0530

    initial commit

---
+++ batch/BATCH_PROGRAM.cbl
@@ -0,0 +1,9 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. BatchProgram.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       COPY COMMON_COPYBOOK1.
+       PROCEDURE DIVISION.
+       BEGIN.
+           DISPLAY "Batch program processing..."
+       END PROGRAM BatchProgram.
---
+++ online/ONLINE_PROGRAM.cbl
@@ -0,0 +1,9 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. OnlineProgram.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       COPY COMMON_COPYBOOK2.
+       PROCEDURE DIVISION.
+       BEGIN.
+           DISPLAY "Online program processing..."
+       END PROGRAM OnlineProgram.
